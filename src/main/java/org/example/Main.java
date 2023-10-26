d892eda7d8a12ffbac94a05bd56287bb82a0be69
Angelo-Pedott-apel <87554178+Angelo-Pedott-apel@users.noreply.github.com>
2023-10-26 18:10:48 -0300

    Added an EpisodeRepository class for handling custom queries

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,8 @@
 package org.example;
 
-import jakarta.persistence.EntityManager;
-
+import java.util.ArrayList;
 import java.util.List;
 
-import static jakarta.persistence.Persistence.createEntityManagerFactory;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
@@ -12,13 +10,12 @@ public class Main {
 
 
     public static void main(String[] args) {
-        var factory = createEntityManagerFactory("JOJOMAP");
+        List<Integer> partList = new ArrayList<>();
+        partList.add(1);
+        partList.add(2);
+        partList.add(3);
 
-        EntityManager em = factory.createEntityManager();
-
-        List<Episode> matchingEpisodes =
-                em.createQuery("SELECT b from Episode b where 1=1", Episode.class)
-                        .getResultList();
+        List<Episode> matchingEpisodes = EpisodeRepository.findMatchingEpisodes(partList);
 
         ClosestEpisode closestEpisode = EpisodeService.findClosestFromCoord(0,0, matchingEpisodes);
 
