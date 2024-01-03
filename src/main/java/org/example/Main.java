222067c2190c037319ab43abcf9fe970575b8548
Angelo-Pedott-apel <87554178+Angelo-Pedott-apel@users.noreply.github.com>
2024-01-03 10:13:24 -0300

    Added new arguments for the html request headers

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,6 +9,27 @@ public class Main {
 
 
     public static void main(String[] args) {
+        options("/*",
+                (request, response) -> {
+
+                    String accessControlRequestHeaders = request
+                            .headers("Access-Control-Request-Headers");
+                    if (accessControlRequestHeaders != null) {
+                        response.header("Access-Control-Allow-Headers",
+                                accessControlRequestHeaders);
+                    }
+
+                    String accessControlRequestMethod = request
+                            .headers("Access-Control-Request-Method");
+                    if (accessControlRequestMethod != null) {
+                        response.header("Access-Control-Allow-Methods",
+                                accessControlRequestMethod);
+                    }
+
+                    return "OK";
+                });
+
+        before((request, response) -> response.header("Access-Control-Allow-Origin", "*"));
 
         get("/Request", (request, response) -> new RequestQuery(request).responseJSON());
 
